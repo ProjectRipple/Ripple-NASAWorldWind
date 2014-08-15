ccae865b216e317a4c3dd7ef306b74e2efa8c61f
Pawel Grzebala <p.grzebala@gmail.com>
2014-08-15 14:54:15 -0400

    Added some comments

--- src/pg/ripple/nasa/ww/RippleWW.java
+++ src/pg/ripple/nasa/ww/RippleWW.java
@@ -1,37 +1,28 @@
 package pg.ripple.nasa.ww;
 
-import java.util.ArrayList;
-import java.util.Date;
-
 import gov.nasa.worldwind.Configuration;
-import gov.nasa.worldwind.geom.Angle;
 import gov.nasa.worldwind.geom.LatLon;
 import gov.nasa.worldwind.geom.Position;
+
+import java.util.ArrayList;
 
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import pg.ripple.nasa.HTMLBalloon.cloudletBalloon.CloudletInfoManager;
+import pg.ripple.nasa.ww.RippleApplication.RippleFrame;
 import ds.ripple.common.XML.Event;
-import ds.ripple.common.XML.FeedItem.ItemType;
-import ds.ripple.common.XML.XMLMessage;
-import ds.ripple.common.XML.Producer.ProducerType;
-import ds.ripple.common.XML.XMLMessage.XMLMessageBuilder;
 import ds.ripple.sub.Publisher;
 import ds.ripple.sub.PublisherList;
 import ds.ripple.sub.PublisherListListener;
 import ds.ripple.sub.RippleEventListener;
 import ds.ripple.sub.Subscriber;
 import ds.ripple.sub.exceptions.InvalidURLException;
-import ds.ripple.sub.exceptions.XMLMissingArgumentException;
-import pg.ripple.nasa.HTMLBalloon.cloudletBalloon.CloudletInfoManager;
-import pg.ripple.nasa.HTMLBalloon.patientBalloon.PatientBalloonNotifier;
-import pg.ripple.nasa.ww.RippleApplication.RippleFrame;
 
 public class RippleWW {
 	
 	static Subscriber subscriber;
 	static RippleFrame rippleFrame;
-	static PatientBalloonNotifier ballonNotifier;
 	
 	static CloudletInfoManager cloudletManager = null;
 	
@@ -46,17 +37,33 @@ public class RippleWW {
     	
     	// load world wind configuration
     	Configuration.insertConfigurationDocument("pg/ripple/nasa/config/worldwind.xml");
+    	
     	// start the NASA World Wind
     	rippleFrame = RippleApplication.start("Ripple World Wind Application", RippleFrame.class);
     	cloudletManager = CloudletInfoManager.getInstance(rippleFrame);
     	
+    	/***************************************************************/
+    	/*The IP address below is the Directory Services server address*/
+    	/***************************************************************/
     	// start subscriber
     	subscriber = new Subscriber("tcp://localhost", new NASAPublisherListListener());
     	subscriber.connect();
 	}
 	
-	public static void createPatientBalloon(String balloonId, Position position, String ballonName){
-		rippleFrame.addBalloon("linkToPatientBalloon.html", balloonId, position, ballonName);
+	/**
+	 * Creates patient balloon (and its icon) with a "transparent" circle around
+	 * the balloon's icon.
+	 * 
+	 * @param balloonId
+	 *            - balloon id must be the same as patient id
+	 * @param position
+	 *            - position of the balloon's icon
+	 * @param balloonName
+	 *            - balloon name (will be displayed as text next to the
+	 *            balloon's icon)
+	 */
+	public static void createPatientBalloon(String balloonId, Position position, String balloonName){
+		rippleFrame.addBalloon("linkToPatientBalloon.html", balloonId, position, balloonName);
 		rippleFrame.addCloudLetCircle(250, new LatLon(position.getLatitude(), position.getLongitude()), "cloudlet");
 	}
 	
@@ -64,10 +71,17 @@ public class RippleWW {
 		ArrayList<Publisher> publisherList = new ArrayList<Publisher>();
 		ArrayList<String> producerList = new ArrayList<String>();
 		
+		/**
+		 * Called when a publisher list is updated at the Directory Services
+		 * server in the Ripple-Cloud. Publisher list contains currently active
+		 * publishers.
+		 */
 		@Override
 		public void publisherListUpdate(PublisherList pl) {
-			System.out.println("Got update: number of publishers" + pl.getPublishers().length); 
-			
+			System.out.println("Got update form the Directory Services server: number of publishers"
+							+ pl.getPublishers().length);
+
+			// unsubscribe from all publishers
 			for (Publisher p : publisherList) {
 				try {
 					subscriber.unsubscriberFromRippleEvents(p.getURL());
@@ -77,6 +91,7 @@ public class RippleWW {
 			}
 			publisherList.clear();
 			
+			// subscribe to all available publishers
 			for (Publisher p : pl.getPublishers()) {
 				publisherList.add(p);
 				try {
@@ -85,12 +100,20 @@ public class RippleWW {
 					e.printStackTrace();
 				}
 			}
-			System.out.println("I am subscribed to " + publisherList.size() + " publishers");
+			
+			System.out.println("Subscribed to " + publisherList.size() + " publishers");
 		}
 		
-		public void subscribeToPublisher(Publisher p) {
-			subscriber.subscriberForRippleEvents(p.getURL(), new RippleEventListener() {
+		/**
+		 * This method subscribes to Ripple-Events of a given publisher
+		 * @param publihser
+		 */
+		public void subscribeToPublisher(Publisher publihser) {
+			subscriber.subscriberForRippleEvents(publihser.getURL(), new RippleEventListener() {
 				
+				/**
+				 * Called when a message from the publisher arrives.
+				 */
 				@Override
 				public void publishedEvent(Event event) {
 					//System.out.println(event);
